f8e663a9808516c6b566f79c3c705b709d21fb84
Torsten Braun <tbraun@hex00r.de>
2017-07-22 23:05:22 +0200

    3

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,10 +1,2 @@
 require "spec"
 require "../src/ts3_crystal"
-
-Spec.before_each do
-  @server = Ts3Crystal.server_connect({:username => "serveradmin", :password => "password"})
-end
-
-Spec.after_each do
-  @server.disconnect
-end
--- spec/ts3_crystal_spec.cr
+++ spec/ts3_crystal_spec.cr
@@ -3,7 +3,9 @@ require "./spec_helper"
 describe Ts3Crystal do
   context "#server_connect" do
     it "this test should fail" do
-      puts @server.version
+      server = Ts3Crystal.server_connect({:username => "serveradmin", :password => "password"})
+      puts server.version
+      server.disconnect
       true.should eq(false)
     end
   end
