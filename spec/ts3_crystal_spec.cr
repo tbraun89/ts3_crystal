8e1a6e73e5558526fec8feb2383cff36d7206756
Torsten Braun <tbraun@hex00r.de>
2017-07-22 23:23:38 +0200

    4

--- spec/ts3_crystal_spec.cr
+++ spec/ts3_crystal_spec.cr
@@ -2,14 +2,21 @@ require "./spec_helper"
 
 describe Ts3Crystal do
   context "#server_connect" do
-    it "this test should fail" do
+    it "should not fail with a valid connection" do
       server = Ts3Crystal.server_connect({:username => "serveradmin", :password => "password"})
-      puts server.version
       server.disconnect
-      true.should eq(false)
     end
+
+    it "should fail with an invalid password" do
+      expect_raises(Ts3Crystal::ConnectionRefused, "Username and password do not match, login failed.") do
+        Ts3Crystal.server_connect({:username => "serveradmin", :password => "wrong_password"})
+      end
+    end
+
+    # TODO add more specs
   end
 
   context "#client_connect" do
+    # TODO test client connection
   end
 end
